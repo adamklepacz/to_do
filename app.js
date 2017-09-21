862b3b355072d49d843676ded6249b6d7c1d3559
Adam Klepacz <adamklepacz92@gmail.com>
2017-09-21 12:09:22 +0200

    update
    indentation formatting

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 /* TO-DO APP 
-** Author: Adam Klepacz
-** Playground app in plain JS
-** 2017
-*/
-
+ ** Author: Adam Klepacz
+ ** Playground app in plain JS
+ ** 2017
+ */
 document.getElementById('creationTaskForm').addEventListener('submit', saveTask);
 
 //event listeneres for "check task" section
-document.getElementById('showHomeTasks').addEventListener('click', function(){
+document.getElementById('showHomeTasks').addEventListener('click', function() {
 	fetchList('homeTasks');
 }, true);
-document.getElementById('showWorkTasks').addEventListener('click', function(){
+document.getElementById('showWorkTasks').addEventListener('click', function() {
 	fetchList('workTasks');
 }, true);
-document.getElementById('showOtherTasks').addEventListener('click', function(){
+document.getElementById('showOtherTasks').addEventListener('click', function() {
 	fetchList('otherTasks');
 }, true);
 
@@ -28,24 +27,24 @@ fetchTaskCount();
 
 //unique id for task
 function makeId() {
-  let text = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let text = "";
+	let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++) {
+	for(var i = 0; i < 5; i++) {
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 		text += Math.floor(Math.random() * 99999);
 	}
-  return text;
+	return text;
 }
 
 function handleTaskDelete(e) {
 	//event for IE
 	e = event || window.event;
 	e.target = e.target || e.srcElement;
-	
+
 	let clickedButton = e.target,
-			clickedTask = e.target.closest('li.taskItem');
-	
+		clickedTask = e.target.closest('li.taskItem');
+
 	//Climb up the document tree, check if clickedButton is a <button> and 
 	//does it have deleteTask class
 	while(clickedButton) {
@@ -61,10 +60,10 @@ function handleTaskDone(e) {
 	//event for IE
 	e = event || window.event;
 	e.target = e.target || e.srcElement;
-	
+
 	let clickedButton = e.target,
-			clickedTask = e.target.closest('li.taskItem');
-	
+		clickedTask = e.target.closest('li.taskItem');
+
 	//Climb up the document tree, check if clickedButton is a <button> and
 	//does it have doneTask class
 	while(clickedButton) {
@@ -78,19 +77,19 @@ function handleTaskDone(e) {
 
 function saveTask(e) {
 	e.preventDefault(); //prevent page reload
-	
+
 	if(avoidSpacedString()) {
 		document.getElementById('taskArea').style.display = "block";
-	
+
 		let taskTitle = document.getElementById('taskTitle').value,
-				taskPriority = document.getElementById('taskPriority').value,
-				taskCategory = document.getElementById('taskCategory').value += "Tasks",
-				taskId = makeId(),
-				isTaskDone = false, //on the very beginning newTask is always NOT DONE(false)
+			taskPriority = document.getElementById('taskPriority').value,
+			taskCategory = document.getElementById('taskCategory').value += "Tasks",
+			taskId = makeId(),
+			isTaskDone = false, //on the very beginning newTask is always NOT DONE(false)
 
-				creationTaskForm = document.getElementById('creationTaskForm'),
+			creationTaskForm = document.getElementById('creationTaskForm'),
 
-				newTask = {};	//init empty object for storing new task
+			newTask = {}; //init empty object for storing new task
 
 		newTask = {
 			title: taskTitle,
@@ -100,11 +99,9 @@ function saveTask(e) {
 			isTaskDone: isTaskDone //boolean
 		};
 
-
 		let homeTasksArr = [],
-				workTaskArr = [],
-				otherTaskArr = [];
-
+			workTaskArr = [],
+			otherTaskArr = [];
 
 		//if localStorage is empty
 		if(localStorage.getItem(taskCategory) === null) {
@@ -154,11 +151,11 @@ function saveTask(e) {
 				//re-set it up to local storage
 				localStorage.setItem('otherTasks', JSON.stringify(otherTaskArr));
 			}
-		} 
-		
+		}
+
 		fetchList(taskCategory);
-		
-	}	else {
+
+	} else {
 		return;
 	}
 	creationTaskForm.reset();
@@ -168,34 +165,34 @@ function saveTask(e) {
 //fetch list using itemKeyCategory form localstorage
 function fetchList(taskCategory) {
 	let tasksArr = JSON.parse(localStorage.getItem(taskCategory)),
-	
-	//get outputs for category and result(task list)
-			resultCategory = document.getElementById('taskCategoryOutput'),
-			result = document.getElementById('taskOutput');
-			
+
+		//get outputs for category and result(task list)
+		resultCategory = document.getElementById('taskCategoryOutput'),
+		result = document.getElementById('taskOutput');
+
 	if(tasksArr) {
 		result.innerHTML = '';
 		resultCategory.innerHTML = taskCategory;
-		
+
 		//display taskArea
 		document.getElementById('taskArea').style.display = "block";
-		
+
 		for(let i = 0; i < tasksArr.length; i++) {
 			let title = tasksArr[i].title,
-					priority = tasksArr[i].priority,
-					category = tasksArr[i].category,
-					id = tasksArr[i].id,
-					isDone = tasksArr[i].isTaskDone,
-					
-					taskBorder;  //task border
-					
+				priority = tasksArr[i].priority,
+				category = tasksArr[i].category,
+				id = tasksArr[i].id,
+				isDone = tasksArr[i].isTaskDone,
+
+				taskBorder; //task border
+
 			//check priority and set border color
 			if(priority === "High") taskBorder = 'border border-danger';
 			if(priority === "Medium") taskBorder = 'border border-warning';
 			if(priority === "Low") taskBorder = 'border border-info';
-			
+
 			result.innerHTML +=
-					`
+				`
 						<li id="${id}" class="list-group-item mb-3 taskItem ${category} ${taskBorder}">${title}
 							<div class="btn-group float-right">
 								<button id="${id}" class="btn btn-secondary doneTask">Done</button>
@@ -213,59 +210,59 @@ function fetchList(taskCategory) {
 
 function crossThroughTask(id, isDone) {
 	let currentTask = document.getElementById(id),
-			taskCategory;
-	
+		taskCategory;
+
 	if(isDone) {
 		currentTask.style.textDecoration = "line-through";
 	} else {
-		currentTask.style.textDecoration = "none";	
+		currentTask.style.textDecoration = "none";
 	}
 }
 
 function doneTask(clickedTask, clickedButton) {
 	let buttonId = clickedButton.getAttribute('id'),
-			clickedTaskId = clickedTask.getAttribute('id'),
-			isDone, //boolean
-	
-			taskArr = [],
-			taskCategory = "";
-	
+		clickedTaskId = clickedTask.getAttribute('id'),
+		isDone, //boolean
+
+		taskArr = [],
+		taskCategory = "";
+
 	if(clickedTask.classList.contains('homeTasks')) {
 		taskArr = JSON.parse(localStorage.getItem('homeTasks'));
 		taskCategory = 'homeTasks';
-		
+
 		for(let i = 0; i < taskArr.length; i++) {
 			if(taskArr[i].id === clickedTaskId) {
-				isDone =	taskArr[i].isTaskDone = true;
-				
+				isDone = taskArr[i].isTaskDone = true;
+
 				localStorage.setItem('homeTasks', JSON.stringify(taskArr));
 				crossThroughTask(clickedTaskId, isDone);
 			}
 		}
 	}
-	
+
 	if(clickedTask.classList.contains('workTasks')) {
 		taskArr = JSON.parse(localStorage.getItem('workTasks'));
 		taskCategory = 'workTasks';
-		
+
 		for(let i = 0; i < taskArr.length; i++) {
 			if(taskArr[i].id === clickedTaskId) {
 				isDone = taskArr[i].isTaskDone = true;
-				
+
 				localStorage.setItem('workTasks', JSON.stringify(taskArr));
 				crossThroughTask(clickedTaskId, isDone);
 			}
 		}
 	}
-	
+
 	if(clickedTask.classList.contains('otherTasks')) {
 		taskArr = JSON.parse(localStorage.getItem('otherTasks'));
 		taskCategory = 'otherTasks';
-		
+
 		for(let i = 0; i < taskArr.length; i++) {
 			if(taskArr[i].id === clickedTaskId) {
 				isDone = taskArr[i].isTaskDone = true;
-				
+
 				localStorage.setItem('otherTasks', JSON.stringify(taskArr));
 				crossThroughTask(clickedTaskId, isDone);
 			}
@@ -276,58 +273,58 @@ function doneTask(clickedTask, clickedButton) {
 
 function deleteTask(clickedTask, clickedButton) {
 	let buttonId = clickedButton.getAttribute('id'),
-			clickedTaskId = clickedTask.getAttribute('id'),
-			
-			taskArr = [],
-			taskCategory = "";
-	
+		clickedTaskId = clickedTask.getAttribute('id'),
+
+		taskArr = [],
+		taskCategory = "";
+
 	clickedTask.classList.add("bg-danger");
-	
+
 	setTimeout(function() {
-			if(clickedTask.classList.contains('homeTasks')) {
-		taskArr = JSON.parse(localStorage.getItem('homeTasks'));
-		taskCategory = 'homeTasks';
-		
-		for(let i = 0; i < taskArr.length; i++) {
-			if(taskArr[i].id === clickedTaskId) {
-				taskArr.splice(taskArr[i], 1); //remove matched element
-				localStorage.setItem('homeTasks', JSON.stringify(taskArr));
+		if(clickedTask.classList.contains('homeTasks')) {
+			taskArr = JSON.parse(localStorage.getItem('homeTasks'));
+			taskCategory = 'homeTasks';
+
+			for(let i = 0; i < taskArr.length; i++) {
+				if(taskArr[i].id === clickedTaskId) {
+					taskArr.splice(taskArr[i], 1); //remove matched element
+					localStorage.setItem('homeTasks', JSON.stringify(taskArr));
+				}
 			}
 		}
-	}
-	
-	if(clickedTask.classList.contains('workTasks')) {
-		taskArr = JSON.parse(localStorage.getItem('workTasks'));
-		taskCategory = 'workTasks';
-		
-		for(let i = 0; i < taskArr.length; i++) {
-			if(taskArr[i].id === clickedTaskId) {
-				taskArr.splice(taskArr[i], 1); //remove matched element
-				localStorage.setItem('workTasks', JSON.stringify(taskArr));
+
+		if(clickedTask.classList.contains('workTasks')) {
+			taskArr = JSON.parse(localStorage.getItem('workTasks'));
+			taskCategory = 'workTasks';
+
+			for(let i = 0; i < taskArr.length; i++) {
+				if(taskArr[i].id === clickedTaskId) {
+					taskArr.splice(taskArr[i], 1); //remove matched element
+					localStorage.setItem('workTasks', JSON.stringify(taskArr));
+				}
 			}
 		}
-	}
-	
-	if(clickedTask.classList.contains('otherTasks')) {
-		taskArr = JSON.parse(localStorage.getItem('otherTasks'));
-		taskCategory = 'otherTasks';
-		
-		for(let i = 0; i < taskArr.length; i++) {
-			if(taskArr[i].id === clickedTaskId) {
-				taskArr.splice(taskArr[i], 1); //remove matched element
-				localStorage.setItem('otherTasks', JSON.stringify(taskArr));
+
+		if(clickedTask.classList.contains('otherTasks')) {
+			taskArr = JSON.parse(localStorage.getItem('otherTasks'));
+			taskCategory = 'otherTasks';
+
+			for(let i = 0; i < taskArr.length; i++) {
+				if(taskArr[i].id === clickedTaskId) {
+					taskArr.splice(taskArr[i], 1); //remove matched element
+					localStorage.setItem('otherTasks', JSON.stringify(taskArr));
+				}
 			}
 		}
-	}
-	cleanLocalStorage(taskCategory);
-	fetchList(taskCategory);
-	fetchTaskCount();
+		cleanLocalStorage(taskCategory);
+		fetchList(taskCategory);
+		fetchTaskCount();
 	}, 500);
 }
 
 function cleanLocalStorage(taskCategory) {
 	let taskArr = JSON.parse(localStorage.getItem(taskCategory));
-	
+
 	if(!taskArr.length) {
 		localStorage.removeItem(taskCategory);
 	}
@@ -336,10 +333,10 @@ function cleanLocalStorage(taskCategory) {
 function avoidSpacedString() {
 	//detect spaced string
 	let taskTitle = document.getElementById('taskTitle').value;
-	
-	if (!taskTitle.replace(/\s/g, '').length) {
+
+	if(!taskTitle.replace(/\s/g, '').length) {
 		alert('Empty spaces? Nope.');
-    return false;
+		return false;
 	}
 	return true;
 }
@@ -347,18 +344,18 @@ function avoidSpacedString() {
 function fetchTaskCount() {
 	//count home tasks
 	let homeTaskArr = JSON.parse(localStorage.getItem('homeTasks')) || [],
-			workTaskArr = JSON.parse(localStorage.getItem('workTasks')) || [],
-			otherTasksArr = JSON.parse(localStorage.getItem('otherTasks')) || [],
-			
-			homeTaskCount = homeTaskArr.length,
-			workTaskCount = workTaskArr.length,
-			otherTaskCount = otherTasksArr.length;
-	
+		workTaskArr = JSON.parse(localStorage.getItem('workTasks')) || [],
+		otherTasksArr = JSON.parse(localStorage.getItem('otherTasks')) || [],
+
+		homeTaskCount = homeTaskArr.length,
+		workTaskCount = workTaskArr.length,
+		otherTaskCount = otherTasksArr.length;
+
 	//fetch badges
 	let homeTaskCounter = document.getElementById('homeTaskCounter'),
-			workTaskCounter = document.getElementById('workTaskCounter'),
-			otherTaskCounter = document.getElementById('otherTaskCounter');
-	
+		workTaskCounter = document.getElementById('workTaskCounter'),
+		otherTaskCounter = document.getElementById('otherTaskCounter');
+
 	homeTaskCounter.innerHTML = homeTaskCount;
 	workTaskCounter.innerHTML = workTaskCount;
 	otherTaskCounter.innerHTML = otherTaskCount;
